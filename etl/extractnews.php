a9a6c6be19add91b59b1a20296f40aa8901f74d4
sm-fhgr <saurabh.mishra@stud.fhgr.ch>
2024-10-08 12:31:59 +0200

    zzz

--- etl/extractnews.php
+++ etl/extractnews.php
@@ -11,8 +11,22 @@ if($e = curl_error($ch)) {
     echo $e;
 }
 else {
+    include_once('config.php');
+    $pdo=new PDO($dsn, $username, $password, $options);
     $newsfeed = json_decode($response, true);
-    print_r($newsfeed);
+    echo  "<pre>";
+    print_r($newsfeed["feed"]);
+    echo "</pre>";
+    foreach($newsfeed["feed"] as $feed_item) {
+        $title = $feed_item['title'];
+        $url = $feed_item['url'];
+        $source = $feed_item['source'];
+        $time_published = $feed_item['time_published'];
+        $overall_sentiment_label = $feed_item['overall_sentiment_label'];
+        $overall_sentiment_score = $feed_item['overall_sentiment_score'];
+        $stmt = $pdo->prepare("INSERT INTO Feed (title, url, source, time_published, overall_sentiment_label, overall_sentiment_score) VALUES (:title, :url, :source, :time_published, :overall_sentiment_label, :overall_sentiment_score)");
+        $stmt->execute(['title' => $title, 'url' => $url, 'source' => $source, 'time_published' => $time_published, 'overall_sentiment_label' => $overall_sentiment_label, 'overall_sentiment_score' => $overall_sentiment_score]);
+    }
 }
 
 return $newsfeed;
--- etl/extractprice.php
+++ etl/extractprice.php
@@ -2,7 +2,7 @@
   $curl = curl_init();
 
   curl_setopt_array($curl, array(
-    CURLOPT_URL => "https://api.coinranking.com/v2/coin/Qwsogvtv82FCd/history",
+    CURLOPT_URL => "https://api.coinranking.com/v2/coin/Qwsogvtv82FCd/history?timePeriod=24h",
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_ENCODING => "",
     CURLOPT_MAXREDIRS => 10,
@@ -10,23 +10,45 @@
     CURLOPT_FOLLOWLOCATION => true,
     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
     CURLOPT_CUSTOMREQUEST => "GET",
-      CURLOPT_HTTPHEADER => array(
+    CURLOPT_HTTPHEADER => array(
         "x-access-token: coinrankingfa5471a783d222de584205fd8a365972765878998246bded"
-      ),
+    ),
   ));
 
   $response = curl_exec($curl);
 
-  if($e = curl_error($curl)) {
+  if ($e = curl_error($curl)) {
     echo $e;
-}
-else {
-    $price = json_decode($response, true);
-    print_r($price);
-}
+  } else {
+    include_once('config.php');
+    $pdo = new PDO($dsn, $username, $password, $options);
+    $priceHistory = json_decode($response, true);
 
-return $price;
+    // echo "<pre>";
+    // print_r($priceHistory);
+    // echo "</pre>";
 
-curl_close($ch); 
+    // Get the price and timestamp from the API response with a loop that goes through array priceHistory["data"]["history"][x] where x is the index of the price and timestamp
 
-  ?>
+    foreach($priceHistory["data"]["history"] as $priceHistory) {
+      $price = $priceHistory["price"];
+      $timestamp = $priceHistory["timestamp"];
+      // echo "<pre>";
+      // print_r($price);
+      // echo "</pre>";
+
+  
+
+    // Convert the Unix timestamp to SQL TIMESTAMP format (YYYY-MM-DD HH:MM:SS)
+    $sqlTimestamp = date('Y-m-d H:i:s', $timestamp);
+
+    // Insert the price and the converted timestamp into the database
+    $stmt = $pdo->prepare("INSERT INTO PriceHistory (price, timestamp) VALUES (:price, :timestamp)");
+    $stmt->execute(['price' => $price, 'timestamp' => $sqlTimestamp]);
+    }
+  }
+
+  curl_close($curl);
+
+  return $price;
+?>
